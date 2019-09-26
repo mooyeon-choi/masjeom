294599e9cf0654d9b6859193d68ebb3452da1df4
Ssong10 <leesy1403@naver.com>
2019-09-27 00:38:39 +0900

    190926 | Add SchoolName_Api, SchoolKeyword
    Get schoolcode, schooltype From Api using SchoolName
    Make SchoolKeyword (SchoolStyle - abbreviations)

--- bixby-workspace/example.findmenu/code/findMenu.js
+++ bixby-workspace/example.findmenu/code/findMenu.js
@@ -1,15 +1,29 @@
-module.exports.function = function findMenu (month,date) {
+module.exports.function = function findMenu (schoolname, schoolkeyword ,month,date) {
   // const request = require('request');
   const console = require('console');
-  var response = http.getUrl('https://schoolmenukr.ml/api/high/N100000260?year=2019&month='+month,{format:'json'})
+  console.log(schoolname);
+  if (schoolkeyword){
+    var response1 = http.getUrl('https://schoolcodekr.ml/api?q='+ schoolname+schoolkeyword,{format:'json'});
+  }
+  else{
+    var response1 = http.getUrl('https://schoolcodekr.ml/api?q='+ schoolname,{format:'json'});
+  }
+  console.log(response1)
+  var schoolcode = response1['school_infos'][0]['code']
+  var schooltype = response1['school_infos'][0]['type']
+  console.log(schoolcode);
+  console.log(schooltype);
+ 
+
+  var response2 = http.getUrl('https://schoolmenukr.ml/api/' + schooltype + '/'+ schoolcode + '?year=2019&month='+ month,{format:'json'});
   // request(url, (err, res, body) => {
   //   var json = JSON.parse(body);
-  response = response['menu'][date-1]['lunch']
-    for (var i = 0;i< response.length;i++){
-      console.log(response[i]);
+  response2 = response2['menu'][date-1]['lunch']
+    for (var i = 0;i< response2.length;i++){
+      console.log(response2[i]);
     }
   
   return {
-    result:response
+    result:response2
   }
 }
