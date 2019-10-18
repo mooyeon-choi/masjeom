de97bc31685bb863cb5838ff053502d013d15670
sjy9412 <sjy9412@gmail.com>
2019-10-18 21:41:05 +0900

    191018 | Add allergy_detail, View interaction
    Add allergy_detail
    WeeklyResult to DailyResult

--- bixby-workspace/example.findmenu/code/findMenu.js
+++ bixby-workspace/example.findmenu/code/findMenu.js
@@ -1,29 +1,100 @@
-module.exports.function = function findMenu (schoolname, schoolkeyword ,month,date) {
-  // const request = require('request');
+module.exports.function = function findMenu(dateTimeExpression, schools, meal, date, schoolname) {
+  var dates = require('dates')
   const console = require('console');
-  console.log(schoolname);
-  if (schoolkeyword){
-    var response1 = http.getUrl('https://schoolcodekr.ml/api?q='+ schoolname+schoolkeyword,{format:'json'});
-  }
-  else{
-    var response1 = http.getUrl('https://schoolcodekr.ml/api?q='+ schoolname,{format:'json'});
-  }
-  console.log(response1)
-  var schoolcode = response1['school_infos'][0]['code']
-  var schooltype = response1['school_infos'][0]['type']
-  console.log(schoolcode);
-  console.log(schooltype);
- 
+  const http = require('http');
+  var month = dates.ZonedDateTime.now().getMonth()
+  var day = dates.ZonedDateTime.now().getDay()
+  var meal = 'lunch'
+  var my_allergy = ['2', '9']
 
-  var response2 = http.getUrl('https://schoolmenukr.ml/api/' + schooltype + '/'+ schoolcode + '?year=2019&month='+ month,{format:'json'});
-  // request(url, (err, res, body) => {
-  //   var json = JSON.parse(body);
-  response2 = response2['menu'][date-1]['lunch']
-    for (var i = 0;i< response2.length;i++){
-      console.log(response2[i]);
+  if (meal == '아침') {
+    meal = 'breakfast'
+  }
+  else if (meal == '저녁') {
+    meal = 'dinner'
+  }
+
+  if (date) {
+    day = date
+  }
+
+  var schoolcode = schools.code[0]
+  var schooltype = schools.type[0]
+  if (dateTimeExpression) {
+    // dateTimeExpression이 기간으로 들어올때
+    if (dateTimeExpression.dateInterval) {
+      var result = new Array();
+      var startmonth = dateTimeExpression.dateInterval.start.month
+      var endmonth = dateTimeExpression.dateInterval.end.month
+      var day = dateTimeExpression.dateInterval.end.day
+      if (startmonth == endmonth) {
+        var response = http.getUrl('https://schoolmenukr.ml/api/' + schooltype + '/' + schoolcode + '?hideAllergy=true&month=' + startmonth, { format: 'json' });
+        result = response['menu'].slice(day - 7, day - 2);
+      }
+      else {
+        var response = http.getUrl('https://schoolmenukr.ml/api/' + schooltype + '/' + schoolcode + '?hideAllergy=true&month=' + startmonth, { format: 'json' });
+        result = response['menu'].slice(day - 7);
+        var response = http.getUrl('https://schoolmenukr.ml/api/' + schooltype + '/' + schoolcode + '?hideAllergy=true&month=' + endmonth, { format: 'json' });
+        result = result.concat(response['menu'].slice(0, day - 2));
+      }
+      for (var i = 0; i < result.length; i++) {
+        result[i] = {
+          date: result[i]['date'],
+          menuName: result[i][meal]
+        }
+      }
+      console.log(result)
+      return {
+        schoolname: schools.name[0],
+        result: result
+      }
     }
-  
+    // dateTimeExpression 특정 날짜 하루 일때
+    else if (dateTimeExpression.date) {
+      month = dateTimeExpression.date.month
+      day = dateTimeExpression.date.day
+    }
+  }
+  var allergyList = new Array();
+  var myAllergy = new Array();
+  var menus = {};
+  var menu = ['menu1', 'menu2', 'menu3', 'menu4', 'menu5', 'menu6', 'menu7', 'menu8', 'menu9']
+  var response = http.getUrl('https://schoolmenukr.ml/api/' + schooltype + '/' + schoolcode + '?hideAllergy=true&month=' + month, { format: 'json' });
+  var allergy = http.getUrl('https://schoolmenukr.ml/api/' + schooltype + '/' + schoolcode + '?month=' + startmonth, { format: 'json' });
+  allergy = allergy['menu'][day - 1][meal];
+  console.log(allergy)
+  for (var i = 0; i < allergy.length; i++) {
+    allergyList[i] = allergy[i].replace(/[^0-9\.]/g, "").split('.').slice(0, -1);
+    for (var j = 0; j < my_allergy.length; j++) {
+      if (allergyList[i].length > 0 && allergyList[i].indexOf(my_allergy[j]) != -1) {
+        myAllergy[i] = true
+      }
+    }
+    if (!myAllergy[i]) {
+      myAllergy[i] = false
+    }
+  }
+  let db = require('data/allergy.js')
+  let allergyName = db.allergy
+  for (var i = 0; i < allergyList.length; i++) {
+    for (var j = 0; j < allergyList[i].length; j++) {
+      allergyList[i][j] = allergyName[allergyList[i][j]]
+    }
+  }
+  for (var i = 0; i < menu.length; i++) {
+    menus[menu[i]] = {
+      menuName: response['menu'][day - 1][meal][i],
+      allergy: allergyList[i],
+      myAllergy: myAllergy[i]
+    }
+  }
+  console.log(date)
   return {
-    result:response2
+    date: day,
+    schoolname: schools.name[0],
+    menus: menus
   }
 }
+
+
+
